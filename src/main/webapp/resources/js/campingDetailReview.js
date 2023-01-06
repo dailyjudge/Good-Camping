d2de48fd36b52cdc8247155c6f2d1419765a5e33
dailyjudge <jmk961202@gmail.com>
2023-01-06 17:51:54 +0900

    resolve Exception : [Review-update, campingSite - defaultImg , like - not login]

--- src/main/webapp/resources/js/campingDetailReview.js
+++ src/main/webapp/resources/js/campingDetailReview.js
@@ -15,11 +15,15 @@ function deleteReview(no) {
 	
 }
 
-function openHiddenContents(no) {
+function openHiddenContents(no, star) {
 	console.log(no);
 	if(confirm('리뷰를 정말 수정하시겠습니까?')) {
 		$('.review-visible-' + no).css('display', 'none');
 		$('.review-hidden-' + no).css('display', 'block');
+		$('.review-hidden-category-' + no).css('display','block');
+		$('.star-hidden-' + no).css('display', 'block');
+		
+		$('.star-hidden-' + no + ' span').css('width', star * 20 + '%');
 	}
 }
 
@@ -31,21 +35,58 @@ function closeHiddenContents(no) {
 	}
 }
 
+// 별점 관련 이벤트
 const drawStar = (target) => {
     document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
-    console.log($('#camping-review-star').val());
+    
+    console.log(target.value);
+}
+
+const drawStar2 = (target) => {
+	let className = $(target).attr('class');
+	//27
+	no = className.slice(className.length - 2, className.length);
+	
+	document.querySelector('.star-hidden-' + no + ' span').style.width = `${target.value * 10}%`;
 }
 
 function updateReview(no) {
 	if(confirm('리뷰를 정말 수정하시겠습니까?')) {
+		// 변경된 내용
 		let title = $('.review-hidden-' + no + '-title').val();
 		let content = $('.review-hidden-' + no + '-content').val();
+		let star = $('.camping-review-star-' + no).val() / 2;
+		star = star.toFixed(1);
+		
+		// 기존 내용
+		let originTitle = $('.review-visible-' + no + '-title').text();
+		let originContent = $('.review-visible-' + no + '-content').text();
+		let originStar = $('.camping-review-star-origin-' + no).val();
+		
+		// 입력된 내용이 없다면
+		if(title == "") {
+			alert('제목을 입력하세요.');
+			$('.review-hidden-' + no + '-title').focus();
+			return;
+		}
+		if(content == "") {
+			alert('내용을 입력하세요.');
+			$('.review-hidden-' + no + '-content').focus();
+			return;
+		}
+		// 기존 데이터와 같다면
+		if(originTitle == title && originContent == content && originStar == star) {
+			aletr('변경된 내용이 없습니다.');
+			$('.review-hidden-' + no + '-title').focus();
+			return;
+		}
 		
 		$.ajax({
 			url: 'do.update.review',
 			data: {"cr_no" : no,
 				   "cr_title" : title,
-				   "cr_content" : content},
+				   "cr_content" : content,
+				   "cr_star" : star},
 			type: "GET"	   
 		}).done(function(res) {
 			if(res == 1) {
@@ -54,7 +95,22 @@ function updateReview(no) {
 				
 				$('.review-visible-' + no).css('display', 'block');
 				$('.review-hidden-' + no).css('display', 'none');
+				
+				$('.review-hidden-category-' + no).css('display','none');
+				$('.star-hidden-' + no).css('display', 'none');
+				
+				// 값 변경
+				$('.review-user-star-' + no).text(star);
+				// 사진 변경
+				$('.camping-content-star-img-' + no).attr("src", "resources/star/star-" + star + ".jpeg");
+				$('.star-hidden-' + no + ' span').css('width', star * 20 + '%');
+				
+				
+				
+				
 				alert("리뷰 수정이 완료되었습니다.");
+				
+				
 			}
 		})
 	}
